345d9f85fbfcfbbe2117a536b169d08f05fde88e
tratchapong <tratchapong@gmail.com>
2024-10-28 15:18:53 +0700

    start authRoute

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const authRoute = require('./routes/authRoute')
 const app = express()
 
+app.use('/auth', authRoute)
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=>console.log('Server on', port))
