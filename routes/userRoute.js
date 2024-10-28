5e8acc1ba05cd9cb4a757f0d2cf4a58344311724
jeangcodecamp <jeang.codecamp@gmail.com>
2024-10-28 15:28:04 +0700

    userRoute start

---
+++ routes/userRoute.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const userRoute = express.Router()
+
+userRoute.post('/info', ()=>{})
+
+module.exports = userRoute
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const userRoute = require('./routes/userRoute')
 const app = express()
 
+app.use('/user', userRoute)
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=>console.log('Server on', port))
